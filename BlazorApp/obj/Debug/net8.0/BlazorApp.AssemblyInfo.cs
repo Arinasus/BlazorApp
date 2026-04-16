5f0d01d502a6e74e939b9ac47803542a6d16f252
Arinana <allyut12zk@gmail.com>
2026-04-16 15:08:09 +0300

    Add editor  for lections

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c4b848f1f5a0a062e0a3d5485da6916ec15eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac418734514ae7bae16b27c41e8a11073fa8558")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
