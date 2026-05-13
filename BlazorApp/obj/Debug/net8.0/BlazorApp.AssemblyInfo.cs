4e1867fbe3d1a7755d761df2db4b269c422143c9
Arinana <allyut12zk@gmail.com>
2026-05-14 00:05:40 +0300

    Fix bugs

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40da3bccfcc4313eef9908f58565eda174f1bd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a1064ae73bbb027437e1c187185a4e86bd391a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
